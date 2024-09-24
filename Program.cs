35389b0622895510547da8d3cc74c4fde5491c6e
Ofer Zadikario <ofer.zadikario@gmail.com>
2024-09-24 22:54:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace InheritanceLessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lesson plan
+            //1. SHow the story of the Shape classes and draw the UML diagram
+            //2. Show the code of the Shape classes
+            //3. SHow the Square code and explain the concept
+            //4. Write a program that creates objects of each class and prints the area of each object
+            //5. Explain the concept of polymorphism, including upcasting and downcasting. Write a program that demonstrates it.
+            //6. Explain the concept of reflection and teach the is operator. Write a program that demonstrates it.
+            //7. Write a program that creates an array of Shape objects and prints the area of each object (after downcastign to the appropriate type)
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceLessons
+{
+    class Shape
+    {
+        private string color;
+        public Shape(string color)
+        {
+            this.color = color;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+        public void SetColor(string color) 
+        {
+            this.color = color;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        private double baseLength;
+        private double heightLength;
+        
+        public Triangle(string color, double baseLength, double heightLength) : base(color)
+        {
+            this.baseLength = baseLength;
+            this.heightLength = heightLength;
+        }
+
+        public double GetArea()
+        {
+            return this.baseLength * this.heightLength;
+        }
+
+        public double GetBaseLength()
+        {
+            return this.baseLength;
+        }
+
+        public double GetHeightLength()
+        {
+            return this.heightLength;
+        }
+    }
+
+    class  Rectangle:Shape
+    {
+        private double length, width;
+        public Rectangle(string color, double length, double width) : base(color)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double GetLength()
+        {
+            return this.length;
+        }
+
+        public double GetWidth()
+        {
+            return this.width;
+        }
+        public double GetArea()
+        {
+            return this.length * this.width;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (this.length + this.width);
+        }
+    }
+
+    class Square : Rectangle
+    {
+        public Square(string color, double side) : base(color, side, side)
+        {
+        }
+    }
+}
